46e16187cae06a940660dea7da59f7e1c2e931ed
houyaohui <rancho1992.494@gmail.com>
2017-08-24 16:45:35 +0800

    针对后加载页面处理
    body加touch事件，每次touchmove移动查找是否有新的mlink标签出现

--- src/index.js
+++ src/index.js
@@ -11,23 +11,37 @@
 	// 短链
 	var mlink_link = 'https://anwka9.mlinks.cc/AcRD';//短链地址
 
+	document.addEventListener('touchmove',function(e){
+		if( mlinksLenght < document.querySelectorAll('.app_open_mlink').length ){
+			app_open_mlinks = document.querySelectorAll('.app_open_mlink');
+			mlinksLenght = app_open_mlinks.length;
+			AppMlinks()
+		}
+	})
 	// 所有打开APP按钮
 	var app_open_mlinks = document.querySelectorAll('.app_open_mlink'),
-		App_mlink_options = [];
+		mlinksLenght = app_open_mlinks.length;
+	function AppMlinks(){
+		app_open_mlinks = document.querySelectorAll('.app_open_mlink');
+		var App_mlink_options = [];
 
-	for(var i = 0;i<app_open_mlinks.length;i++){
-	    var ele = app_open_mlinks[i];
-	    App_mlink_options[i] = {};
-	    if( ele.dataset['doubledeck'] ){         // 楼中楼
-	        App_mlink_options[i].mlink = mlink_link +'?type=doublefloor&id='+ window.btoa(location.href) +'&title='+ encodeURI(document.title);
-	    }else{         // 其他
-	        App_mlink_options[i].mlink = mlink_link +'?type='+ MLINK_DATA.type +'&id='+ MLINK_DATA.id;
-	    }
-	    App_mlink_options[i].button = ele;
+		for(var i = 0;i<app_open_mlinks.length;i++){
+		    var ele = app_open_mlinks[i];
+		    App_mlink_options[i] = {};
+		    if( MLINK_DATA.type == 'doublefloor' ){         // 楼中楼
+		        App_mlink_options[i].mlink = mlink_link +'?type=doublefloor&id='+ window.btoa(location.href) +'&title='+ encodeURI(document.title);
+		    }else{         // 其他
+		        App_mlink_options[i].mlink = mlink_link +'?type='+ MLINK_DATA.type +'&id='+ MLINK_DATA.id;
+		    }
+		    App_mlink_options[i].button = ele;
+		}
+
+		new Mlink(App_mlink_options);
 	}
+
 	var setTimer = setTimeout(function(){
-	    new Mlink(App_mlink_options);
-	    clearTimeout(setTimer);
-	},500)
+		AppMlinks();
+		clearTimeout(setTimer);
+	},1e3)
 
 }()
